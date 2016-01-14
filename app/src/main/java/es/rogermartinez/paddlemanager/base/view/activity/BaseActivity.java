aceac185babf6bc1ed4c2bd1c4c702ded361abad
Roger Martinez Teres <roger.martinez@infojobs.net>
2016-01-14 09:30:15 +0100

    Button back in toolbar

--- app/src/main/java/es/rogermartinez/paddlemanager/base/view/activity/BaseActivity.java
+++ app/src/main/java/es/rogermartinez/paddlemanager/base/view/activity/BaseActivity.java
@@ -52,8 +52,6 @@ public abstract class BaseActivity extends AppCompatActivity implements ViewErro
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 
